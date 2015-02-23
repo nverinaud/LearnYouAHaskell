3bfb270faa95a68638215f01e92b1ca6bb246732
Nicolas VERINAUD <nicolas@chloro.eu>
2015-02-23 14:06:35 +0100

    Move to higher order functions.

--- playground.hs
+++ playground.hs
@@ -1,3 +1,7 @@
+-- 
+-- Haskell Playground
+-- 
+
 cylinder :: (RealFloat a) => a -> a -> a
 cylinder r h =
 	let 
@@ -5,3 +9,27 @@ cylinder r h =
 		topArea = pi * r ^ 2
 	in
 		sideArea + 2 * topArea
+
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+
+repeat' :: a -> [a]
+repeat' a = [a] ++ repeat' a
+
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) =
+  let 
+    smallerThanXSorted = quicksort' [a | a <- xs, a <= x]
+    biggerThanXSorted = quicksort' [a | a <- xs, a > x]
+  in 
+    smallerThanXSorted ++ [x] ++ biggerThanXSorted
+
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (x:xs) = 1 + length' xs
